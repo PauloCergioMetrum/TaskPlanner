edc2bf74ff86808114684c60291f08237909fb47
Paulo Cergio <110677926+74196312@users.noreply.github.com>
2024-04-02 09:23:02 -0300

    Correção de Modelos

--- TaskPlannerMetrum/Model/PMAcquisitionMade.cs
+++ TaskPlannerMetrum/Model/PMAcquisitionMade.cs
@@ -6,12 +6,13 @@ namespace TaskPlannerMetrum.Model
     {
         public string ID { get; set; }
         public int StatusAcquistionID { get; set; }
-        public int Amount { get; set; }
+        public double Amount { get; set; }
         public string AquisitionPlannedID { get; set; }
-        public double Value { get; set; }
+        public double? Value { get; set; }
         public DateTime DateAcquisition { get; set; }
         public DateTime DateAcquisitionDelivery { get; set; }
-        public double TotalMade { get; set; }
+        public double TotalCost { get; set; }
+        // public double TotalMade { get; set; }
 
     }
 }
