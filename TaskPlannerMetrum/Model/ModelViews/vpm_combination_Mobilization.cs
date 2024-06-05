bbf36365f84f37b49e46b37e56b15710b0d24b8e
brunomelgaco <bruno.costa@memt.com.br>
2024-06-05 08:28:59 -0300

    Update - Mudança no model

--- TaskPlannerMetrum/Model/ModelViews/vpm_combination_Mobilization.cs
+++ TaskPlannerMetrum/Model/ModelViews/vpm_combination_Mobilization.cs
@@ -15,15 +15,8 @@ namespace TaskPlannerMetrum.Model.ModelViews
         public double CountOthers { get; set; }
 
         public int ContractID { get; set; }
-
-        public double CountMobilizationMade { get; set; }
         public double CountAccommodationMade { get; set; }
         public double CountFoodMade { get; set; }
         public double CountAirTransportMade { get; set; }
-        public DateTime DateStart { get; set; }
-        public DateTime DateEnd { get; set; }
-
-        [StringLength(255)]
-        public string IDMade { get; set; }
     }
 }
