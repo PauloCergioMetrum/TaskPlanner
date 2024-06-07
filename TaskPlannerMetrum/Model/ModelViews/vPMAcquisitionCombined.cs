257ae44d8eeb21da5c57531abf6ed5dc02bb1354
Paulo Cergio <110677926+74196312@users.noreply.github.com>
2024-06-07 13:44:10 -0300

    Alter TotalCost

--- TaskPlannerMetrum/Model/ModelViews/vPMAcquisitionCombined.cs
+++ TaskPlannerMetrum/Model/ModelViews/vPMAcquisitionCombined.cs
@@ -8,7 +8,7 @@
         public string Description { get; set; }
         public int ContractID { get; set; }
         public double AmountValue { get; set; }
-        public double TotalCosT { get; set; }
+        public double TotalCost { get; set; }
         public double PredictedTotal { get; set; }
         public double Difference {  get; set; }
     }
