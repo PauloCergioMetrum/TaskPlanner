068e059e7e7d3e2c7ddb613aee55969311a7ba94
Paulo Cergio <110677926+74196312@users.noreply.github.com>
2024-06-11 12:33:33 -0300

    Retornando  o ClienteName

--- TaskPlannerMetrum/Repository/ActivityPlan/ActivityPlanRepository.cs
+++ TaskPlannerMetrum/Repository/ActivityPlan/ActivityPlanRepository.cs
@@ -671,7 +671,6 @@ namespace TaskPlannerMetrum.Repository.ActivityPlan
 
             var scheduleDataList = hoursExecutorsList.Select(s => s.ScheduledDate).Distinct().ToList();
 
- 
             foreach (var schedule in scheduleDataList)
             {
                 var executorList = hoursExecutorsList.Where(s => s.ScheduledDate == schedule).Select(s => s.Executor).Distinct().ToList();
@@ -679,38 +678,41 @@ namespace TaskPlannerMetrum.Repository.ActivityPlan
                 foreach (var executor in executorList)
                 {
                     List<HoursByDay> hoursByDays = new List<HoursByDay>();
-
                     var hoursOnDateForExecutor = hoursExecutorsList
-                        .Where(h => h.ScheduledDate == schedule && h.Executor == executor);
+                       .Where(h => h.ScheduledDate == schedule && h.Executor == executor);
 
-                    Dictionary<string, TimeSpan> totalPlannedHoursByProject = new Dictionary<string, TimeSpan>();
+                    
+                    Dictionary<string, (TimeSpan PlannedHours, string ClientName)> totalPlannedHoursByProject = new Dictionary<string, (TimeSpan PlannedHours, string ClientName)>();
 
                     foreach (var hourOnDate in hoursOnDateForExecutor)
                     {
-                        if (totalPlannedHoursByProject.ContainsKey(hourOnDate.Project))
+                      
+                        if (totalPlannedHoursByProject.TryGetValue(hourOnDate.Project, out var existingEntry))
                         {
-                            totalPlannedHoursByProject[hourOnDate.Project] += TimeSpan.Parse(hourOnDate.PlannedManHours);
+                         
+                            existingEntry.PlannedHours += TimeSpan.Parse(hourOnDate.PlannedManHours);
                         }
                         else
                         {
-                            totalPlannedHoursByProject[hourOnDate.Project] = TimeSpan.Parse(hourOnDate.PlannedManHours);
+                           
+                            totalPlannedHoursByProject.Add(hourOnDate.Project, (TimeSpan.Parse(hourOnDate.PlannedManHours), hourOnDate.ClientName));
                         }
                     }
 
-                    
+
                     foreach (var kvp in totalPlannedHoursByProject)
                     {
                         hoursByDays.Add(new HoursByDay
                         {
-                            PlannedManHours = $"{(int)kvp.Value.TotalHours:00}:{kvp.Value.Minutes:00}",
+                            PlannedManHours = $"{(int)kvp.Value.PlannedHours.TotalHours:00}:{kvp.Value.PlannedHours.Minutes:00}",
                             Project = kvp.Key,
-                            ClientName = hoursOnDateForExecutor.FirstOrDefault().ClientName, 
+                            ClientName = kvp.Value.ClientName,
                         });
                     }
 
-
                     TimeSpan totalHours = totalPlannedHoursByProject.Values
-                        .Aggregate(TimeSpan.Zero, (total, time) => total.Add(time));
+                       .Select(v => v.PlannedHours)
+                       .Aggregate(TimeSpan.Zero, (total, time) => total.Add(time));
 
                     string resultTotalHours = $"{(int)totalHours.TotalHours:00}:{totalHours.Minutes:00}";
 
@@ -720,16 +722,10 @@ namespace TaskPlannerMetrum.Repository.ActivityPlan
                         ScheduledDate = schedule,
                         TotalPlannedHours = resultTotalHours,
                         Hours = hoursByDays,
-                      
-
-
-
-
                     });
                 }
-
-
             }
+
             return hoursDto;
         }
 
