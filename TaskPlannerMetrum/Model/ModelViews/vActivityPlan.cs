122371faccefecddf51453304715b366a2b862a1
Paulo Cergio <110677926+74196312@users.noreply.github.com>
2024-06-21 14:04:29 -0300

    Alterando EquipmentID para aceitar null

--- TaskPlannerMetrum/Model/ModelViews/vActivityPlan.cs
+++ TaskPlannerMetrum/Model/ModelViews/vActivityPlan.cs
@@ -40,11 +40,11 @@ namespace TaskPlannerMetrum.Model.ModelViews
 
         public bool? IsRework { get; set; }
 
-        public string TaskDescription { get; set; }
+        public string? TaskDescription { get; set; }
 
-        public string BusinessUnit {  get; set; }
-        public string MilestonesID { get; set; }
+        public string? BusinessUnit {  get; set; }
+        public string ?MilestonesID { get; set; }
 
-        public int EquipmentID { get; set; }    
+        public int ?EquipmentID { get; set; }    
     }
 }
