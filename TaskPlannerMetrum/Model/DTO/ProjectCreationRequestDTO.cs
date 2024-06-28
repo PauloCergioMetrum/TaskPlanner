e17fe18fd20b82b3c4fe3380fe1042e930d5e003
Paulo Cergio <110677926+74196312@users.noreply.github.com>
2024-06-28 10:17:15 -0300

    Endpoint de Informaçoes Gerais   (TSKP-307
    )

---
+++ TaskPlannerMetrum/Model/DTO/ProjectCreationRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TaskPlannerMetrum.Model.DTO
+{
+    public class ProjectCreationRequestDTO
+    {
+
+        public PM_TAP_General_Info ProjectInfo { get; set; }
+
+        public PM_TAP_Scope Scopes { get; set; }
+
+
+
+    }
+}
