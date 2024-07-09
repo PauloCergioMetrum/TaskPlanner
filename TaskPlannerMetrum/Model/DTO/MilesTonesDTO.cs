32f0681d8cae0aee4eb7c686e10e9d0c0d971005
Paulo Cergio <110677926+74196312@users.noreply.github.com>
2024-07-09 09:40:17 -0300

    TSKP-31  Alterando ExecutedDate para aceitar null

--- TaskPlannerMetrum/Model/DTO/MilesTonesDTO.cs
+++ TaskPlannerMetrum/Model/DTO/MilesTonesDTO.cs
@@ -11,22 +11,17 @@ namespace TaskPlannerMetrum.Model.DTO
 
         public string ID { get; set; }
         public int Baseline { get; set; }
-
         public string Name { get; set; }    
         public int MilestonesID {  get; set; }
         public int ContractID { get; set; }
         public string Description { get; set; }
         public DateTime ScheduledDate { get; set; }
-        public DateTime RescheduledDate { get; set; }
-       
-        public DateTime ExecutedDate { get; set; }
-   
+        public DateTime RescheduledDate { get; set; }       
+        public DateTime? ExecutedDate { get; set; }   
         public int TypeID { get; set; }
-
         public double Value { get; set; }
         public string? TypeMilestonesID { get; set; }
         public int TechLeadID { get; set; }
-
         public int BusinessUnitID { get; set; }
     }
 
