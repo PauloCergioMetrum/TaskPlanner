ae2769dbc7d6da2ab994c689c06af717d460d49b
Paulo Cergio de Almeida Costa Junior <paulo.junior@memt.com.br>
2024-09-23 09:52:06 -0300

    Resolvendo conflitos durante merge de alterações locais e remotas. - Arquivos ajustados manualmente - Conflitos resolvidos em [ActivityPlanBusiness,IActivityPlanBusiness,MSSQLContext,IActivityPlanRepository]

--- TaskPlannerMetrum/Repository/ActivityPlan/IActivityPlanRepository.cs
+++ TaskPlannerMetrum/Repository/ActivityPlan/IActivityPlanRepository.cs
@@ -10,53 +10,26 @@ namespace TaskPlannerMetrum.Repository.ActivityPlan
     public interface IActivityPlanRepository
     {
         public dynamic GetExecutorPlan(string projectId);
-
         public bool Create(TaskPlannerMetrum.Model.ActivityPlan activityPlan);
-
         public List<vActivePlans> FindAllTaskByProject(int MilestonesID ,int ContractID);
         public vActivityPlan GetActivityPlanById(int MilestonesID);
-
-
         Task<List<MilestoneDetailDTO>> GetMilestonesByContractAsync(int contractId);
-
-
-
-
+       // Task<List<MilestoneDetailDTO>> GetMilestonesByContractAsync(int contractId);
         public List<vActivePlans> FindAllTaskByUser(string userId);
         public bool UpdateActivityPlan(vActivityPlan activityPlan);
-
         public bool DeleteId(int id);
-
         public string GetNameProject(int id);
-
         public dynamic GetDepartamentProject(int id);
-
         public dynamic GetUserTask(int departamentID);
-
         public dynamic GetDepTask(UserDepForTask deptask);
-
-
         public bool DuplicateTask(Model.DuplicatTask activityPlan);
-
-
         public bool UpdateRating(RatingUpdate updatetask);
-
         public dynamic GetUserforTask(int id);
-
         public bool UpdateNotes(int taskID, string notesExecut, string notesPlanned, string identifier);
-
         public string UpdateTaskDescription(int taskID, string taskDescription);
-
         public List<vActivePlanBusinessUnit> GetBusinessUnitByContract(int ContractID);
-
         public List<HoursDTO> ExecutorHourForPeriod(HoursExecutorDTO executors);
-
-        // LOCAÇÃO DE EQUIPAMENTOS
-        public List<Equipment> GetAllEquipment();
-
-        Task<List<MilestoneDetailDTO>> GetMilestonesByContractAsync(int contractId);
-
-
+        public List<Equipment> GetAllEquipment();   
         public List<GetEquipamentAvaibilaity> GetEquipamentAvaibilaities(int EquipamentID , DateTime StartDate , DateTime EndDate);    
     }
 }
