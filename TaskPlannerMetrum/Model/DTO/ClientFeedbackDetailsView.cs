580e654e2b5dd6d7e43e689e4c372019a6564bac
Paulo Cergio de Almeida Costa Junior <paulo.junior@memt.com.br>
2024-10-02 13:48:24 -0300

    Retornando  Data de Término das Atividades:

--- TaskPlannerMetrum/Model/DTO/ClientFeedbackDetailsView.cs
+++ TaskPlannerMetrum/Model/DTO/ClientFeedbackDetailsView.cs
@@ -9,18 +9,17 @@ namespace TaskPlannerMetrum.Model.DTO
         public string Contact { get; set; }
         public string Scope { get; set; }
         public DateTime? LatestScheduledDate { get; set; }
-       
         public DateTime? LastContactDate { get; set; }
         public string ClientResponse { get; set; }
         public int ClientRating { get; set; }
         public string ReceivedComplaint { get; set; }
         public int ContractID { get; set; }
-        public DateTime?FeedbackDate {  get; set; } 
-
-
+        public DateTime? FeedbackDate { get; set; }
+        public string MaxScheduledDate { get; set; }
 
 
     }
+
 
 }
 
