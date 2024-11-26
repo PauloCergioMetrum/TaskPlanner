6ca28eed336e41507b5dab5592080028864dbd80
Paulo Cergio de Almeida Costa Junior <paulo.junior@memt.com.br>
2024-11-26 14:18:50 -0300

    TSKP-455 -Status Reports

--- TaskPlannerMetrum/Model/ModelViews/vIndirectcostChart.cs
+++ TaskPlannerMetrum/Model/ModelViews/vIndirectcostChart.cs
@@ -3,11 +3,11 @@
     public class vIndirectcostChart
     {
         public int ContractID { get; set; }
-        public string PlannedId { get; set; }       
-        public string MadeId { get; set; }          
-        public int TypeID { get; set; }          
-        public string TypeDescription { get; set; }
-        public double PlannedTotal { get; set; }     
-        public double MadeTotal { get; set; }
+        public string? PlannedId { get; set; }       
+        public string? MadeId { get; set; }          
+        public int? TypeID { get; set; }          
+        public string? TypeDescription { get; set; }
+        public double? PlannedTotal { get; set; }     
+        public double? MadeTotal { get; set; }
     }
 }
