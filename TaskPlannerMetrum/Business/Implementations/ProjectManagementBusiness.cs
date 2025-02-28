5774d63e1467084cf6d1b96fa67e47cdcd24123d
Estevão Lopes <estevao.correia@hotmail.com>
2025-02-28 08:51:26 -0300

    Fix: mudando model de OrderManagementInfo.cs para resolver total custo previsto e total custo realizado nos cards HH

--- TaskPlannerMetrum/Business/Implementations/ProjectManagementBusiness.cs
+++ TaskPlannerMetrum/Business/Implementations/ProjectManagementBusiness.cs
@@ -938,7 +938,7 @@ namespace TaskPlannerMetrum.Business.Implementations
                 .Select(group => new OrderManagementResult
                 {
                     ContractID = group.Key,
-                    TotalDifferenceExpectedExecuted = (float)group.Sum(x => x.TotalDifferenceExpectedExecuted ?? 0.0),
+                    TotalDifferenceExpectedExecuted = (float)group.Sum(x => x.TotalCostExecuted ?? 0.0),
                     TotalCostHoursExecuted = (float)group.Sum(x => x.TotalCostHoursExecuted ?? 0.0)
                 })
                 .ToList();
