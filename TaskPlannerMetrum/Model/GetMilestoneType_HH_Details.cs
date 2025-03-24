0291d1102df9cd06a2fb6402f1b8a73c647dff79
Paulo Cergio <paulo.junior@memt.com.br>
2025-03-24 15:41:58 -0300

    Bug - TSKP-504 Horas previstas da tap não está correta. Na TAP deve aparecer as horas cadastradas no Avanço Fisico.

--- TaskPlannerMetrum/Model/GetMilestoneType_HH_Details.cs
+++ TaskPlannerMetrum/Model/GetMilestoneType_HH_Details.cs
@@ -1,28 +1,23 @@
-﻿#nullable enable 
+﻿#nullable enable
 using System;
+
+
 namespace TaskPlannerMetrum.Model
 {
     public class GetMilestoneType_HH_Details
     {
-        public string? MilestoneTypeID { get; set; }
+        public string MilestoneTypeID { get; set; }  
         public string? DepartmentName { get; set; }
         public string? DisplacementServiceName { get; set; }
-        public int? DepartmentID { get; set; }
-        public string? MilestonesValueID { get; set; }
-        public int? DisplacementServicesID { get; set; }
-        public int? FunctionID { get; set; }
-        public double? ValueHour { get; set; }
-        public double? HoursExpected { get; set; }
-
-
-        public double? CostHoursExpected { get; set; }
-        public double? CostHoursPlanned { get; set; }
-        public double? CostHoursExecuted { get; set; }
-
-
-
+        public int DepartmentID { get; set; }
+        public string MilestonesValueID { get; set; }
         public string? FunctionName { get; set; }
-        public DateTime? ScheduledDate { get; set; }
-      
+       public double CostHoursExpected { get; set; }   
+        public string? DetailsHH { get; set; }
+        public int ContractID { get; set; }
+         public int? MilestonesID { get; set; }
     }
 }
+
+
+
