be208f780ee07bb61c113441ec8a807acdda9e2a
Paulo Cergio <paulo.junior@memt.com.br>
2025-04-15 16:01:13 -0300

    Fix -Tornar propriedades de ProjectStatusInfo nullable
    As propriedades `id`, `InternalCode`, `Status` e `IsBlocked` da classe `ProjectStatusInfo` foram alteradas para serem do tipo nullable, ou seja, agora elas podem aceitar valores nulos (`null`). Isso foi feito adicionando o caractere `?` após o tipo de cada propriedade.

--- TaskPlannerMetrum/Model/DTO/ProjectStatusInfo.cs
+++ TaskPlannerMetrum/Model/DTO/ProjectStatusInfo.cs
@@ -4,11 +4,11 @@ namespace TaskPlannerMetrum.Model.DTO
 {
     public class ProjectStatusInfo
     {
-        public int id { get; set; }
-        public string InternalCode { get; set; }
-        public int Status { get; set; }
+        public int? id { get; set; }
+        public string? InternalCode { get; set; }
+        public int? Status { get; set; }
         public DateTime? DateRetroactive { get; set; }
-        public bool IsBlocked { get; set; }
+        public bool? IsBlocked { get; set; }
     }
 }
 
