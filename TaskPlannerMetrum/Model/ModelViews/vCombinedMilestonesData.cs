a1180cdf3f56f1478e237afea364271e063883a8
Estevão Lopes <estevao.correia@hotmail.com>
2025-04-24 09:16:08 -0300

    Fix: adiconando ao backend as alterações do grafico de custo de horas de gestão

--- TaskPlannerMetrum/Model/ModelViews/vCombinedMilestonesData.cs
+++ TaskPlannerMetrum/Model/ModelViews/vCombinedMilestonesData.cs
@@ -6,27 +6,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TaskPlannerMetrum.Model.ModelViews
 {
- 
     public class CombinedMilestonesData
     {
-        [Key]   
-        public string? MilestonesValueID { get; set; }
+        [Key]
+        //public string? MilestonesValueID { get; set; }
         public int? ContractID { get; set; }
         public string? FunctionName { get; set; }
-        public string? DepartmentName { get; set; }
         public string? DisplacementServiceName { get; set; }
-       
         public string? DataSource { get; set; } = string.Empty;
 
-
-        public double? Hours { get; set; }
-        public double? TotalMilestones { get; set; }
-        public double? TotalPlannedManHour { get; set; }
         public double? TotalExecutedManHour { get; set; }
         public decimal? CustoPlanejado { get; set; }
         public decimal? CustoExecutado { get; set; }
         public decimal? TotalCustos { get; set; }
-         public double? Soma { get; set; }
         public decimal? SomaServico { get; set; }
     }
 }
