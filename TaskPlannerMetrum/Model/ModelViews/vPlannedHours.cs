b5fcd7adc9621d558f658fc5bcfe100d0382dbd9
JPaulo Cergio <paulo.junior@memt.com.br>
2025-06-10 13:51:03 -0300

    as somas de horas executadas, planejadas e esperadas agora utilizam o operador de coalescência nula (`?? 0`), aumentando a robustez ao lidar com dados ausentes.

--- TaskPlannerMetrum/Model/ModelViews/vPlannedHours.cs
+++ TaskPlannerMetrum/Model/ModelViews/vPlannedHours.cs
@@ -9,18 +9,18 @@ namespace TaskPlannerMetrum.Model.ModelViews
         public Int64 ID { get; set; }
         public int ContractID { get; set; }
        
-        public string ProjectName { get; set; }
-        public string ClientName { get; set; }
+        public string? ProjectName { get; set; }
+        public string? ClientName { get; set; }
 
-        public string DepartmentName { get; set; }
-        public string TechLeader { get; set; }
-        public double ExpectedHour { get; set; }
-        public double PlannedHour { get; set; }
-        public double ExecutedHour { get; set; }
+        public string? DepartmentName { get; set; }
+        public string? TechLeader { get; set; }
+        public double? ExpectedHour { get; set; }
+        public double? PlannedHour { get; set; }
+        public double? ExecutedHour { get; set; }
     
-        public int StatusID { get; set; }
-        public int TechLeaderID { get; set; }
-        //public  string StatusGuarantee {  get; set; }  
+        public int? StatusID { get; set; }
+        public int? TechLeaderID { get; set; }
+        
 
     }
 }
