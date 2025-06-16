cc6eaecad62dfd777691d98cdb0e189cdd3f0a3f
JPaulo Cergio <paulo.junior@memt.com.br>
2025-06-16 19:35:19 -0300

    Bug-
    Atualiza papéis de autorização no UserController
    
    A autorização de acesso foi alterada para incluir o papel 2 na lista de papéis permitidos. Anteriormente, apenas os papéis 4, 1 e 3 eram permitidos.

--- TaskPlannerMetrum/Controllers/UserController.cs
+++ TaskPlannerMetrum/Controllers/UserController.cs
@@ -13,7 +13,7 @@ namespace TaskPlannerMetrum.Controllers
 {
     [ApiController]
     [Route("api/[controller]/v{version:apiVersion}")]
-    [Authorize(Roles = "4,1 ,3")]
+    [Authorize(Roles = "4,1 ,3,2")]
 
     public class UserController : ControllerBase
     {
