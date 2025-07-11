61c207acdc43558fea7e6d4744b48a448abf55ac
Paulo Cergio <paulo.junior@memt.com.br>
2025-07-11 11:51:01 -0300

    Fix- Refatorando o grafico de HH  TSKP-512

--- TaskPlannerMetrum/Model/ModelViews/vMilestonesStatistics.cs
+++ TaskPlannerMetrum/Model/ModelViews/vMilestonesStatistics.cs
@@ -11,12 +11,12 @@ namespace TaskPlannerMetrum.Model.ModelViews
         public double? TotalMilesStone { get; set; }
         public string FunctionName { get; set; }
         public int ContractID { get; set; }
-        public string MilestonesValueID { get; set; }
-        public double? SomaTotalMilesStone { get; set; }
-        public decimal? TotalExecutedManHour { get; set; }
-        public decimal? CustoPlanejado { get; set; }
-        public decimal? CustoExecutado { get; set; }
-        public decimal? CustoTotalPlanejado { get; set; }
-        public decimal? SomaTotalExecutedManHour { get; set; }
+ 
+
+        public double? TotalExecutedManHour { get; set; }
+        public double? CustoPlanejado { get; set; }
+        public double? CustoExecutado { get; set; }
+        public double? CustoTotalPlanejado { get; set; }
+
     }
 }
